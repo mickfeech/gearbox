2b653a4fa3124043ca5efe3c71c5de285a5edad5
mcfeech <christopher_mcfee@keybank.com>
2016-02-23 15:34:44 -0500

    filtering

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,10 @@ require 'rspec/its'
 require 'support/shared_configuration'
 require 'vcr'
 
-#VCR.configure do |c|
-#  c.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
-#  c.hook_into :webmock
-#end
+VCR.configure do |c|
+  c.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
+  c.hook_into :webmock
+  c.filter_sensitive_data('<GEARBOX_AUTH_TOKEN>') {ENV['auth_token']}
+  c.filter_sensitive_data('<GEARBOX_BASE_URL>') {ENV['base_url']}
+  c.filter_sensitive_data('<SERVER>') {ENV['server_names']}
+end
